23cafa4ddb5b5401ebfc9d8d9cf419c87e919b35
Emre <emre@azradex.com>
2026-03-28 16:41:25 +0300

    Github pages setup

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import path from "path";
 
 export default defineConfig({
+    base: "/obsidian-sync-ui",
     plugins: [
         react(),
         tailwindcss()
